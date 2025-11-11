3a13a1bee91d6e3111bc22999311083b0c89b687
w-yas <wataru084622@gmail.com>
2025-11-12 00:22:26 +0900

    ロギング修正

--- back/api/utils/logging.py
+++ back/api/utils/logging.py
@@ -5,12 +5,22 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "default": {"format": "%(asctime)s %(levelname)s %(name)s %(request_id)s %(message)s"}
+        "colored": {
+            "()": "colorlog.ColoredFormatter",
+            "format": "%(log_color)s%(levelname)s%(reset)s: %(asctime)s %(name)s %(request_id)s %(message)s",
+            "log_colors": {
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bg_red",
+            },
+        }
     },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
-            "formatter": "default",
+            "formatter": "colored",
             "level": "INFO",
         }
     },
